3e4135ff0992a1f6a6c032199d12ece3f885fd88
yamanaka kazuki <yamanakakazuki@yamanakaikkinoMacBook-Air.local>
2023-05-27 11:12:51 +0900

    feat: ホーム画面レイアウト更新

--- codeco/lib/main.dart
+++ codeco/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,109 +8,240 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return CupertinoApp(
+      title: 'codeco',
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return const CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('ホーム'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      child: SafeArea(
+        child: _MyHomePageState(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+final List<String> entries = <String>[
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G'
+];
+
+class _MyHomePageState extends StatelessWidget {
+  const _MyHomePageState({super.key});
+
+  @override
+  Widget build(BuildContext context) => CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        iconSize: 24,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            label: 'ホーム',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.qrcode),
+            label: 'QR追加',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.settings_solid),
+            label: '設定',
+          ),
+        ],
+      ),
+      tabBuilder: (BuildContext context, int index) {
+        return CupertinoTabView(
+          builder: (BuildContext context) => CupertinoPageScaffold(
+              child: SingleChildScrollView(
+                  child: Column(children: [
+            Container(
+              height: 150,
+              child: Column(children: [
+                Container(
+                  margin: EdgeInsets.only(top: 30, bottom: 20),
+                  width: 350,
+                  height: 50,
+                  child: CupertinoSearchTextField(
+                    placeholder: 'キーワードを入力して検索',
+                    placeholderStyle: TextStyle(
+                      fontSize: 14,
+                      color: CupertinoColors.systemGrey2,
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: 100,
+                      height: 30,
+                      child: CupertinoButton(
+                        padding: EdgeInsets.zero,
+                        borderRadius: BorderRadius.circular(20),
+                        child: Text(
+                          '期限が近い順',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 12,
+                          ),
+                        ),
+                        onPressed: () {
+                          print('Tap 期限が近い順');
+                        },
+                        color: Colors.blue,
+                      ),
+                    ),
+                    SizedBox(
+                        width: 100,
+                        height: 30,
+                        child: CupertinoButton(
+                          padding: EdgeInsets.zero,
+                          borderRadius: BorderRadius.circular(20),
+                          child: Text(
+                            'お気に入り',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                            ),
+                          ),
+                          onPressed: () {
+                            print('Tap お気に入り');
+                          },
+                          color: Colors.blue,
+                        )),
+                    SizedBox(
+                        width: 100,
+                        height: 30,
+                        child: CupertinoButton(
+                          padding: EdgeInsets.zero,
+                          borderRadius: BorderRadius.circular(20),
+                          child: Text(
+                            '五十音順',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                            ),
+                          ),
+                          onPressed: () {
+                            print('Tap 五十音順');
+                          },
+                          color: Colors.blue,
+                        )),
+                  ],
+                ),
+              ]),
+            ),
+            ListView.builder(
+                shrinkWrap: true,
+                itemCount: 8,
+                itemBuilder: (BuildContext context, int index) {
+                  return CupertinoListTile(
+                    title: Text(
+                      'ここにタイトルが入ります',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    leading: Image.asset(
+                      'assets/images/no-image.jpeg',
+                      height: 120,
+                      width: 80,
+                    ),
+                    leadingSize: 70,
+                    trailing: Wrap(
+                      spacing: 8,
+                      children: [
+                        Icon(Icons.edit,
+                            size: 20, color: CupertinoColors.systemGrey),
+                        Icon(Icons.delete,
+                            size: 20, color: CupertinoColors.systemGrey),
+                      ],
+                    ),
+                    subtitle: Column(
+                      children: [
+                        Container(
+                          alignment: Alignment.topLeft,
+                          margin: EdgeInsets.only(bottom: 10),
+                          child: Text(
+                            '有効期限：2023/12/31',
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                                width: 120,
+                                height: 28,
+                                child: CupertinoButton(
+                                  alignment: Alignment.center,
+                                  padding: EdgeInsets.zero,
+                                  borderRadius: BorderRadius.circular(20),
+                                  child: Text(
+                                    'QRコードを表示',
+                                    style: TextStyle(
+                                      color: Color(0xFF191970),
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  onPressed: () {
+                                    print('Tap 五十音順');
+                                  },
+                                  color: CupertinoColors.systemGrey5,
+                                )),
+                            // 余白
+                            SizedBox(width: 15),
+                            SizedBox(
+                                width: 80,
+                                height: 28,
+                                child: CupertinoButton(
+                                  alignment: Alignment.center,
+                                  padding: EdgeInsets.zero,
+                                  borderRadius: BorderRadius.circular(20),
+                                  child: Text(
+                                    '使った！',
+                                    style: TextStyle(
+                                      color: Color(0xFF191970),
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  onPressed: () {
+                                    print('Tap 五十音順');
+                                  },
+                                  color: CupertinoColors.systemGrey5,
+                                )),
+                          ],
+                        ),
+                      ],
+                    ),
+                    padding: EdgeInsets.all(15),
+                  );
+                }),
+          ]))),
+        );
+      });
 }
